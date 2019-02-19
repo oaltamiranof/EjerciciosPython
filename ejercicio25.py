db8dd84f7c707201e13122262dc158bda809e930
oaltamiranof <47788797+oaltamiranof@users.noreply.github.com>
2019-02-19 11:45:37 -0600

    Add files via upload

---
+++ ejercicio25.py
@@ -0,0 +1,16 @@
+import ex5
+sentence = "All good things come to those who wait."
+words = ex5.break_words(sentence)
+words
+sorted_words = ex5.sort_words(words)
+sorted_words
+ex5.print_first_word(words)
+ex5.print_last_word(words)
+words
+ex5.print_first_word(sorted_words)
+ex5.print_last_word(sorted_words)
+sorted_words
+sorted_words = ex5.sort_sentence(sentence)
+sorted_words
+ex5.print_first_and_last(sentence)
+ex5.print_first_and_last_sorted(sentence)
